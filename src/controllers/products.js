d9887d5802fa9ac3a6475c2118ddbb504fbdd427
Fabiano Júnior <50252807+fabianosantos23@users.noreply.github.com>
2022-07-18 19:39:48 -0300

    add method post

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -5,15 +5,30 @@ async function get(req, res) {
 
   const obj = id ? { _id: id } : null
 
-  if (id) {
-    obj._id = id
-  }
-
   const products = await ProductsModel.find(obj)
 
   res.send(products)
 }
 
+async function post(req, res) {
+  const {
+    name,
+    brand,
+    price
+  } = req.body
+
+  const product = new ProductsModel({
+    name,
+    brand,
+    price
+  })
+
+  product.save()
+
+  res.send()
+}
+
 module.exports = {
   get,
+  post
 }
