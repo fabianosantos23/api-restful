7d54cf58a6fe6a0f9d28cd03842702bf8ce8dc35
Fabiano Júnior <50252807+fabianosantos23@users.noreply.github.com>
2022-07-18 19:24:34 -0300

    add method get

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,19 +1,7 @@
 const router = require('express').Router()
 
-const ProductsController = require('../controllers/products')
+const ProductController = require('../controllers/products')
 
-
-// VERBOS HTTP (4 TIPOS)
-// GET - obter dados
-// POST - enviar/receber dados
-// PUT - atualizar dados
-// DELETE - remover dados
-
-
-
-router.get('/products', ProductsController.get)
-//router.post('/products', ProductsController.post)
-//router.put('/products/:id', ProductsController.put)
-//router.delete('/products/:id', ProductsController.delete)
+router.get('/products/:id?', ProductController.get)
 
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-//const path = require('path')
+//const cors = require('cors')
 
 const db = require('./database/db')
 const routes = require('./routes/routes')
@@ -7,15 +7,14 @@ const routes = require('./routes/routes')
 const app = express()
 
 // conexão com o banco de dados
-db.connect
+db.connect()
 
-// habilita server para receber dados via post (formulário)
-app.use(express.urlencoded({ extended: true }))
+// habilita server para receber dados json
+app.use(express.json())
 
-// definindo rotas
+// definindo as rotas
 app.use('/api', routes)
 
 // executando o servidor
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
-
